ad2872916a9ddcdec566c5d0bfe4a18a9c0809e5
Declan O'Gorman <dogorman@outlook.com>
2020-12-04 17:15:22 +0000

    Day 4 update

--- 4/code_a.py
+++ 4/code_a.py
@@ -1,15 +1,32 @@
+
+import re
+
+def isValid(passport, requiredFields):
+    fields = 0
+    for field in requiredFields:
+        if field in passport: fields += 1
+    if fields == len(requiredFields): return 1
+    return 0
+
+def isValidField(field, value):
+
+
 passports = []
 passport = {}
 with open('./4/input_a.txt', 'r') as f:
     for line in f:
         for field in line.split():
-            passport[field.split(':')[0]] = field.split[1]
-        if line == '': 
+            passport[field.split(':')[0]] = field.split(':')[1]
+        if line.strip() == '': 
             passports.append(passport)
             passport = {}
 passports.append(passport)
 
 requiredFields = ['byr','iyr','eyr','hgt','hcl','ecl','pid']
+validationRule = {{'byr',  }}
 
+valid = 0
 for passport in passports:
-    
+   valid += isValid(passport, requiredFields) 
+
+print(f'Part A: {valid}')
