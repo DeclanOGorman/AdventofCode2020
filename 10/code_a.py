6361e5d90bf68a79b9f30e2cdefb50d4ec18d380
Declan O'Gorman <dogorman@outlook.com>
2020-12-10 07:56:57 +0000

    Day 10

--- 10/code_a.py
+++ 10/code_a.py
@@ -1,6 +1,26 @@
 input = []
 with open('./10/input_a_test.txt', 'r') as f:
-    for line in f: input.append(line.strip())
+    for line in f: input.append(int(line.strip()))
 
-print(f'Part A: {0}')
-print(f'Part B: {0}')
+input.sort()
+diff = [0,0,0,0]
+joltage = 0
+for j in input:
+    diff[j-joltage] += 1
+    joltage = j
+diff[3] += 1
+
+print(f'Part A: {diff[1]*diff[3]}')
+
+def seq(adapters, joltage):
+    a = list(filter(lambda a: a > joltage and a <= joltage + 3, adapters))
+    paths = 0
+    for j in a:
+        ad = list(filter(lambda a: a > j, adapters))
+        paths += seq(ad, j)
+        print(j)
+    if len(a) == 0: 
+        return 1
+    return paths
+
+print(f'Part B: {seq(input,0)}')
