c5e79841c0592530e71609f319d55ef9eced7190
Declan O'Gorman <dogorman@outlook.com>
2020-12-10 15:47:22 +0000

    Day 10

--- 10/code_a.py
+++ 10/code_a.py
@@ -1,6 +1,5 @@
-input = []
-with open('./10/input_b_test.txt', 'r') as f:
-    for line in f: input.append(int(line.strip()))
+with open('./10/input_a.txt', 'r') as f:
+    input = [int(line.strip()) for line in f]
 
 input.sort()
 diff = [0,0,0,0]
@@ -12,24 +11,30 @@ diff[3] += 1
 
 print(f'Part A: {diff[1]*diff[3]}')
 
-def seq(adapters, joltage):
-    a = list(filter(lambda c: c > joltage and c <= joltage + 3, adapters))
-    paths = 0
-    for j in a:
-        paths += seq(list(filter(lambda b: b > j, adapters)), j)
-    if len(a) == 0: 
-        return 1
-    return paths
+input.append(0)
+input.sort()
 
-length = len(input)
-def seq2(adapters, index):
+def splitThrees(adapters):
+    splitInput = [[]]
+    last = 0
+    for i in adapters:
+        if i == last + 3: splitInput.append([i])
+        else: splitInput[len(splitInput)-1].append(i)
+        last = i
+    return splitInput
+
+def seq(adapters, index):
     paths = 0
     i = index + 1
+    length = len(adapters)
     if (i == length): return 1
-    while adapters[i] <= adapters[index] + 3:
-        paths += seq2(adapters, i)
+    while i < length and adapters[i] <= adapters[index] + 3:
+        paths += seq(adapters, i)
         i = i+1
-        if (i == length): return 1
     return paths
 
-print(f'Part B: {seq2(input,0)}')
+total = 1
+for s in splitThrees(input):
+    total *= seq(s,0)
+
+print(f'Part B: {total}')
