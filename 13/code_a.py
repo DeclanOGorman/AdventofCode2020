73c62affd480378976765733eebb3cb54468838b
Declan O'Gorman <dogorman@outlook.com>
2020-12-13 13:24:12 +0000

    Day 13

--- 13/code_a.py
+++ 13/code_a.py
@@ -5,7 +5,7 @@ with open('./13/input_a.txt', 'r') as f:
     busses = [(i, int(a)) for i, a in enumerate(f.readline().strip().split(',')) if a != 'x']
 
 nextBus, arrival = 0, 999
-for bus in busses:
+for i, bus in busses:
     if bus == 1: continue
     time = bus - (timestamp % bus)
     if time < arrival:
