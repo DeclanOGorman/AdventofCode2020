0dfc3aa39a03dfbda406aad4eb82a26a35ded808
Declan O'Gorman <dogorman@outlook.com>
2020-12-22 15:21:41 +0000

    Day 22

--- 22/code_a.py
+++ 22/code_a.py
@@ -2,8 +2,7 @@ with open('./22/input_a.txt') as f:
     input = [int(a.strip()) for a in f if ':' not in a and not a.strip() == '']
     
 def hash(p1, p2, sep = 1):
-    offset = len(p1) + len(p2)*sep
-    return sum([(offset - i) * a for i,a in enumerate(p2 + p1)])
+    return sum([(len(p1) + len(p2)*sep - i) * a for i,a in enumerate(p2 + p1)])
 
 def playGame(p1, p2, game, recurse = False):
     history = []
@@ -11,10 +10,9 @@ def playGame(p1, p2, game, recurse = False):
         if recurse and hash(p1,p2,1000) in history: return True
         history.append(hash(p1,p2,1000))
         c1, c2 = p1[0], p2[0]
-        p1Wins = False
+        p1Wins = c1 > c2
         if recurse and len(p1) > c1 and len(p2) > c2:
             p1Wins = playGame(p1[1:c1+1], p2[1:c2+1], game + 1, True)
-        else: p1Wins = c1 > c2
         if p1Wins: p1.extend([c1, c2])
         else: p2.extend([c2, c1])
         p1, p2 = p1[1:], p2[1:]
